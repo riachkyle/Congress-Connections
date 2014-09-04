3541e42bd881ea056014ce50fe043db8c97ae77f
NickMro <nickmro@gmail.com>
2014-09-04 15:46:33 -0700

    implemented Capitol Words into graph

--- app/serializers/prod_senator_serializer.rb
+++ app/serializers/prod_senator_serializer.rb
@@ -1,3 +1,3 @@
 class ProdSenatorSerializer < ActiveModel::Serializer
-  attributes :bioguide_id, :senator_name, :nm_first, :nm_last, :gender, :party, :state
+  attributes :bioguide_id, :senator_name, :nm_first, :nm_last, :gender, :party, :state, :state_name, :rank, :term_start, :tern_end
 end
