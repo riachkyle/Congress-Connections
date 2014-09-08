16afd4352d9b4ed4b1fd836cf48205e6b38575da
NickMro <nickmro@gmail.com>
2014-09-07 22:54:32 -0700

    reformatted senator info

--- spec/controllers/prod_votes_controller_spec.rb
+++ spec/controllers/prod_votes_controller_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe ProdVotesController, :type => :controller do
 
+	describe 'GET #index' do
+		
+		before do
+			get :index, format: :json
+		end
+
+		it "should succeed" do
+			expect(response).to be_success
+		end
+
+		it "should assign @votes to be all votes" do
+			expect(assigns(:votes).to_json include(ProdVote.all))
+		end
+
+	end
+
 end
