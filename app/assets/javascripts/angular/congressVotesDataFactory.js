8b0d70c2dbde281bab332e751925c74267ce5bcb
NickMro <nickmro@gmail.com>
2014-09-08 21:28:15 -0700

    edited page load error

--- app/assets/javascripts/angular/congressVotesDataFactory.js
+++ app/assets/javascripts/angular/congressVotesDataFactory.js
@@ -3,7 +3,7 @@ congressApp.factory('VotesData', ['$http', function($http){
   var votes = {};
 
   votes.getData = function(){
-    var url = 'http://localhost:3000/prod_votes/';
+    var url = 'http://ccproject.herokuapp.com/prod_votes/';
     var endpoint = url; 
     return $http({ method: 'GET', url: endpoint });
   };
