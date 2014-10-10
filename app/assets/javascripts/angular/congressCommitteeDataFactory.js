15b06147b7795d259cf691dc3a43cdbaa9a06c37
NickMro <nickmro@gmail.com>
2014-10-10 14:51:15 -0700

    changed API

--- app/assets/javascripts/angular/congressCommitteeDataFactory.js
+++ app/assets/javascripts/angular/congressCommitteeDataFactory.js
@@ -3,7 +3,7 @@ congressApp.factory('CommitteeData', ['$http', function($http){
   var committees = {};
 
   committees.getData = function(){
-    var url = 'http://localhost:3000/prod_committee_refs/';
+    var url = 'http://ccproject.herokuapp.com/prod_committee_refs/';
     var endpoint = url; 
     return $http({ method: 'GET', url: endpoint });
   };
