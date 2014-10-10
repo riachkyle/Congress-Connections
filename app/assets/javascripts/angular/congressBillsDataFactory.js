2e955cd1687c8e1f54073fb1ab1f7083e2e9691c
NickMro <nickmro@gmail.com>
2014-10-10 14:49:56 -0700

    added view all button

--- app/assets/javascripts/angular/congressBillsDataFactory.js
+++ app/assets/javascripts/angular/congressBillsDataFactory.js
@@ -3,7 +3,7 @@ congressApp.factory('BillsData', ['$http', function($http){
   var bills = {};
 
   bills.getData = function(){
-    var url = 'http://ccproject.herokuapp.com/prod_bills/';
+    var url = 'http://localhost:3000/prod_bills/';
     var endpoint = url; 
     return $http({ method: 'GET', url: endpoint });
   };
