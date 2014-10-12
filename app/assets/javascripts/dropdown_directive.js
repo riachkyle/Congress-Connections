fdeb468ee44e497cc2c8daa90a93a5a7101fe99f
NickMro <nickmro@gmail.com>
2014-10-12 12:45:43 -0700

    added senator search

---
+++ app/assets/javascripts/dropdown_directive.js
@@ -0,0 +1,10 @@
+// ---------- Dropdown Search Directive ---------- //
+
+//directive created to make a dropdown search box
+congressApp.directive("dropdownSeach", function($window){
+	return{
+		restrict: "EA",
+		template: "<div></div>",
+		
+	}
+})
